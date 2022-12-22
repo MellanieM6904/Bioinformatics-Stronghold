117013c13137278030e8b8cbfe122e34a81f475c
MellanieM6904 <rsmellaniem@gmail.com>
2022-12-21 21:52:24 -0800

    finished

---
+++ Mendel's First Law [COMPLETED].R
@@ -0,0 +1,12 @@
+# Mendel's First Law
+# Name: Mellanie Martin
+# Summary: The following solves the "Mendel's First Law" Rosalind Challenge
+# in the Bioinformatics Stronghold
+# Status: INCOMPLETE
+
+k <- c("kIndv1", "kIndv2") # homozygous dominant
+m <- c("mIndv1", "mIndv2") # heterozygous
+n <- c("nIndv1", "nIndv2")  # homozygous recessive
+
+individuals <- data.frame(k,m,n)
+print(individuals)
--- Mendel's First Law.R
+++
@@ -1,5 +0,0 @@
-# Mendel's First Law
-# Name: Mellanie Martin
-# Summary: The following solves the "Mendel's First Law" Rosalind Challenge
-# in the Bioinformatics Stronghold
-# Status: INCOMPLETE
